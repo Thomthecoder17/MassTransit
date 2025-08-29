1ccfb8529114845af4225d52216a13d7f45f4434
Thomas Roberts <86893368+Thomthecoder17@users.noreply.github.com>
2025-08-28 21:17:12 -0400

    Delete lib/constants.dart

--- lib/constants.dart
+++
@@ -1,6 +0,0 @@
-class Constants {
-  static const String lightMapUrl = '';
-  static const String mapTilerKey = 'FK2tj2Dq5iKOVZJOmK9H';
-
-  static const String otpUrl = 'https://otp-serve-304832659423.us-east1.run.app/otp/gtfs/v1';
-}
